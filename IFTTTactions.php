cd2a568f01bfe532a917567f0e9ca702ddcfbc2d
KiboOst <KiboOst@users.noreply.github.com>
2017-03-21 17:54:45 +0100

    initial commit

---
+++ IFTTTactions.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+
+endpoint for actions (IFTTT or others...)
+
+call:
+http://www.mydomain.com/path/to/action.php?IP=somedyndns:port&action=autoPlay
+
+*/
+
+require($_SERVER['DOCUMENT_ROOT']."/path/to/phpKodi-api.php");
+
+$IP = 'defaultIP'; //if you got several kodi...
+
+if(isset($_GET['IP'])) $IP = $_GET['IP'];
+if(isset($_GET['action']))
+{
+    $action = $_GET['action'];
+    $_Kodi = new Kodi($IP);
+    if (isset($_Kodi->error)) die($_Kodi->error);
+
+    call_user_func($action); //use same name in url and your function
+}
+
+//actions:
+function autoPlay()
+{
+    global $_Kodi;
+    $_Kodi->clearPlayList();
+    $pl = 'special://profile/playlists/mymusic.xsp';
+    $_Kodi->loadPlaylist($pl);
+    $_Kodi->play();
+    $_Kodi->setVolume(45);
+    $_Kodi->setShuffle(true);
+    $_Kodi->setRepeat('all');
+}
+
+?>
---
+++ class/phpKodi-api.php
@@ -0,0 +1,371 @@
+<?php
+/*
+
+https://github.com/KiboOst/php-Kodi-jsonAPI
+
+*/
+
+class Kodi{
+
+	public $_version = "0.1";
+
+	//user functions======================================================
+	//GET
+	public function getActivePlayer()
+	{
+		$jsonString = '{"method":"Player.GetActivePlayers"}';
+		$answer = $this->_request($jsonString);
+		if (isset($answer['error']) ) return array('result'=>null, 'error'=>$answer['error']);
+
+		if (count($answer['result'])>0)
+		{
+			$this->_playerid = $answer['result'][0]['playerid'];
+			$this->_playerType = $answer['result'][0]['type'];
+			return $this->_playerid;
+		}
+		return array('error'=>'No active player.');
+	}
+
+	public function getPlayerItem($playerid=null)
+	{
+		if ( !isset($playerid) ) $playerid = $this->getActivePlayer();
+		if ( is_array($playerid) ) return $playerid;
+
+		$jsonString = '{
+						"method":"Player.GetItem",
+						"params":{
+									"properties": ["title", "album", "artist", "duration", "file"],
+									"playerid": '.$playerid.'
+								}
+						}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function getPlayList($playlistid=null) //0: music, 1: video, 2:picture
+	{
+		if ( !isset($playlistid) ) $playlistid = $this->getActivePlayer();
+		if ( is_array($playlistid) ) $playlistid = 0;
+
+		if ($playlistid == 0)
+		{
+			$jsonString = '{"method":"Playlist.GetItems",
+						"params":{
+									"properties": ["title", "album", "artist", "duration"],
+									"playlistid": 0 }
+						}';
+		}
+		else
+		{
+			$jsonString = '{"method":"Playlist.GetItems",
+						"params":{
+									"properties": ["runtime", "showtitle", "season", "title", "artist"],
+									"playlistid": 1 }
+						}';
+		}
+
+		return $this->_request($jsonString);
+	}
+
+	public function getDirectory($folder, $type=0)
+	{
+		$folder = urlencode($folder);
+
+		if ($type == 0) $type = 'music';
+		if ($type == 1) $type = 'video';
+		if ($type == 2) $type = 'picture';
+
+		$jsonString = '{"method":"Files.GetDirectory",
+						"params":{"directory":"'.$folder.'",
+						"media":"'.$type.'"
+						}}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function getVolume()
+	{
+		$jsonString = '{
+						"method":"Application.GetProperties",
+						"params":{"properties": ["volume"]}
+						}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function getTime($playerid=null) { return $this->PlayerGetProperties('time', $playerid); }
+	public function getShuffle($playerid=null) { return $this->PlayerGetProperties('shuffled', $playerid); }
+	public function getRepeat($playerid=null) { return $this->PlayerGetProperties('repeat', $playerid); }
+
+	//SET
+	public function play($playlistid=null)
+	{
+		if ( !isset($playlistid) ) $playlistid = $this->getActivePlayer();
+		if ( is_array($playlistid) ) $playlistid = 0;
+
+		$jsonString = '{
+						"method":"Player.Open",
+						"params":{ "item": { "playlistid": '.$playlistid.', "position": 0 } }
+						}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function stop($playerid=null)
+	{
+		if ( !isset($playerid) ) $playerid = $this->getActivePlayer();
+		if ( is_array($playerid) ) $playerid = 0;
+
+		$jsonString = '{
+						"method":"Player.Stop",
+						"params":{"playerid":'.$playerid.'}
+						}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function openFile($file) //will always answer OK even if file doesn't exist!!
+	{
+		$file = urlencode($file);
+		$jsonString = '{"method":"Player.Open",
+						"params":{"item":{"file":"'.$file.'"}}}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function openDirectory($folder)
+	{
+		$folder = urlencode($folder);
+		$jsonString = '{"method":"Player.Open",
+						"params":{"item":{"directory":"'.$folder.'"}}}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function clearPlayList($playlistid=null)
+	{
+		if ( !isset($playlistid) ) $playlistid = $this->getActivePlayer();
+		if ( is_array($playlistid) ) $playlistid = 0;
+
+		$jsonString = '{"method":"Playlist.Clear",
+						"params":{"playlistid":'.$playlistid.'}
+						}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function loadPlaylist($playlist, $type=0)
+	{
+		$playlist = urlencode($playlist);
+
+		if ($type == 0) $media = 'music';
+		if ($type == 1) $media = 'video';
+		if ($type == 2) $media = 'picture';
+
+		$jsonString = '{"method": "Playlist.Add",
+						"params":{"playlistid":'.$type.',
+								  "item":{"directory": "'.$playlist.'", "media": "'.$media.'"}
+								}
+						}';
+
+		return $this->_request($jsonString, 30);
+	}
+
+	public function addPlayListDir($folder=null, $playlistid=null)
+	{
+		$folder = urlencode($folder);
+
+		if ( !isset($playlistid) ) $playlistid = $this->getActivePlayer();
+		if ( is_array($playlistid) ) $playlistid = 0;
+
+		$jsonString = '{"method":"Playlist.Add",
+						"params":{
+								"playlistid":'.$playlistid.', "item": {"directory":"'.$folder.'"}
+								}
+						}';
+
+		return $this->_request($jsonString, 30);
+	}
+
+	public function addPlayListFile($file=null, $playlistid=null)
+	{
+		$file = urlencode($file);
+
+		if ( !isset($playlistid) ) $playlistid = $this->getActivePlayer();
+		if ( is_array($playlistid) ) $playlistid = 0;
+
+		$jsonString = '{"method":"Playlist.Add",
+						"params":{
+								"playlistid":'.$playlistid.', "item": {"file":"'.$file.'"}
+								}
+						}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function togglePlayPause($playerid=null)
+	{
+		if ( !isset($playerid) ) $playerid = $this->getActivePlayer();
+		if ( is_array($playerid) ) return $playerid;
+
+		$jsonString = '{"method":"Player.PlayPause",
+						"params":{
+									"playerid": '.$playerid.'
+								}
+						}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function setShuffle($value=true, $playerid=null) //"true" / "false"
+	{
+		if ( !isset($playerid) ) $playerid = $this->getActivePlayer();
+		if ( is_array($playerid) ) return $playerid;
+
+		$set = ( ($value == true) ? 'true' : 'false' );
+
+		$jsonString = '{"method":"Player.SetShuffle",
+						"params":{
+									"playerid": '.$playerid.',
+									"shuffle":'.$set.'
+								}
+						}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function setRepeat($value="all", $playerid=null) //one, all, off
+	{
+		if ( !isset($playerid) ) $playerid = $this->getActivePlayer();
+		if ( is_array($playerid) ) return $playerid;
+
+		$jsonString = '{"method":"Player.SetRepeat",
+						"params":{
+									"playerid": '.$playerid.',
+									"repeat":"'.$value.'"
+								}
+						}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function setVolume($level=30)
+	{
+		$jsonString = '{"method":"Application.SetVolume",
+						"params":{"volume":'.$level.'}
+						}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function volumeInc()
+	{
+		$jsonString = '{"method":"Application.SetVolume",
+				"params":{"volume": "increment"}
+				}';
+
+		return $this->_request($jsonString);
+	}
+
+	public function volumeDec()
+	{
+		$jsonString = '{"method":"Application.SetVolume",
+				"params":{"volume": "decrement"}
+				}';
+
+		return $this->_request($jsonString);
+	}
+
+	//internal functions==================================================
+	protected function PlayerGetProperties($prop, $playerid)
+	{
+		$currentPlayer = $this->getActivePlayer();
+		if ( !isset($playerid) ) $playerid = $currentPlayer;
+		if ( is_array($playerid) ) return $playerid;
+
+		if ( $currentPlayer != $playerid ) return array('error'=>"Player ID ".$playerid." isn't active!");
+
+		$jsonString = '{"method":"Player.GetProperties",
+						"params":{"properties": ["'.$prop.'"], "playerid": '.$playerid.'}
+						}';
+
+		return $this->_request($jsonString);
+	}
+
+	//calling functions===================================================
+	public function sendJson($jsonString) //testing and custom json request purpose
+	{
+		return $this->_request($jsonString);
+	}
+
+	protected function _request($jsonString, $timeout=3)
+	{
+		if (!isset($this->_curlHdl))
+		{
+			$this->_curlHdl = curl_init();
+			curl_setopt($this->_curlHdl, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($this->_curlHdl, CURLOPT_FOLLOWLOCATION, true);
+
+			curl_setopt($this->_curlHdl, CURLOPT_CONNECTTIMEOUT, 3);
+			curl_setopt($this->_curlHdl, CURLOPT_TIMEOUT, 3);
+		}
+
+		curl_setopt($this->_curlHdl, CURLOPT_TIMEOUT, $timeout);
+
+		$json = json_decode($jsonString, true);
+		$json['jsonrpc'] = '2.0';
+		$json['id'] = $this->_POSTid;
+		$this->_POSTid++;
+
+		$url = "http://".$this->_IP."/jsonrpc?request=".json_encode($json);
+		curl_setopt($this->_curlHdl, CURLOPT_URL, $url);
+
+		$answer = curl_exec($this->_curlHdl);
+		if(curl_errno($this->_curlHdl))
+		{
+		    return array('error'=>curl_error($this->_curlHdl));
+		}
+
+		if ($answer == false)
+		{
+			return array('error'=>"Couldn't reach Kodi device.");
+		}
+
+		$answer = json_decode($answer, true);
+		if (isset($answer['error']) ) return array('result'=>null, 'error'=>$answer['error']);
+		return array('result'=>$answer['result']);
+	}
+
+	function __construct($IP)
+	{
+		$IP = str_replace('http://', '', $IP);
+		$this->_IP = $IP;
+
+		$var = $this->getActivePlayer();
+		if (isset($var['error']) ) $this->_error = $var['error'];
+	}
+
+	public $_IP;
+	public $_error;
+	public $_playerid;
+	public $_playerType;
+
+	protected $_curlHdl = null;
+	protected $_POSTid = 0;
+
+	/*
+	playerid 0: music
+	palyerid 1: video
+	palyerid 2: picture
+
+	playlist 0: current music playlist
+	playlist 1: current video playlist
+
+	http://kodi.wiki/view/JSON-RPC_API/v8
+
+	*/
+
+//Kodi end
+}
+
+?>
